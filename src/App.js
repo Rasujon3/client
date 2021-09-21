d88fb7a3d1cb62a137faa36c8178f2aa4f94c494
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-09-22 02:32:33 +0600

    Appointment Done
    End of Module : 55

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-
+import React, { createContext, useState } from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import Home from './components/Home/Home/Home';
+import Appointment from './components/Appointment/Appointment/Appointment';
+import Login from './components/Login/Login/Login';
+import Dashboard from './components/Dashboard/Dashboard/Dashboard';
+import AllPatients from './components/AllPatients/AllPatients/AllPatients';
+
+export const UserContext = createContext();
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <div>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
       <Router>
         <Switch>
           <Route exact path="/">
             <Home></Home>
           </Route>
+          <Route path="/appointment">
+            <Appointment></Appointment>
+          </Route>
+          <Route path="/dashboard/appointment">
+            <Dashboard></Dashboard>
+          </Route>
+          <Route path="/dashboard/allPatients">
+            <AllPatients></AllPatients>
+          </Route>
+          
+          <Route path="/login">
+            <Login></Login>
+          </Route>
         </Switch>
       </Router>
-    </div>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCGceKI_vChOWgdtSIRayUIKpEHCdRtDSE",
+    authDomain: "burj-al-arab-sujon.firebaseapp.com",
+    projectId: "burj-al-arab-sujon",
+    storageBucket: "burj-al-arab-sujon.appspot.com",
+    messagingSenderId: "1011902534214",
+    appId: "1:1011902534214:web:ca918652cd31abfeecb047"
+  };
+  
+export default firebaseConfig;
